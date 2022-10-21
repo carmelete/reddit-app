cb914601e33bd84b6f8f67705d4841cc73c8c932
carmelete96 <carmenortegarico@gmail.com>
2022-10-21 09:33:32 +0200

    feat: add styles to the button subreddits component

--- src/components/Subreddits/Subreddits.js
+++ src/components/Subreddits/Subreddits.js
@@ -2,12 +2,24 @@ import React from "react";
 
 export function Subreddits() {
     return (
-        <article className="h-full p-4 mt-4 mr-4 text-left border border-gray-300 rounded-md shadow-lg">
-            <h2 className="mb-2 text-xl font-bold">Subreddits</h2>
-            <ul>
+        <article className="flex flex-col p-4 mt-4 mr-4 text-left border border-gray-300 rounded-md shadow-lg h-12/12">
+            <h2 className="mb-2 text-xl font-bold h-1/12">Subreddits</h2>
+            <ul className="h-11/12">
                 <li>
-                    <button type="button" className="p-2 font-bold text-gray-600 hover:bg-gray-200 active:bg-blue-400">
-                        <i class="bi bi-circle text-2xl mr-2"></i>
+                    <button type="button" className="w-full p-2 font-bold text-left text-gray-600 hover:bg-gray-100 active:bg-blue-100 active:border-l-4 active:border-l-blue-700 active:text-blue-700">
+                        <i className="mr-2 text-2xl bi bi-circle"></i>
+                        Home
+                    </button>
+                </li>
+                <li>
+                    <button type="button" className="w-full p-2 font-bold text-left text-gray-600 hover:bg-gray-100 active:bg-blue-100 active:border-l-4 active:border-l-blue-700 active:text-blue-700">
+                        <i className="mr-2 text-2xl bi bi-circle"></i>
+                        Home
+                    </button>
+                </li>
+                <li>
+                    <button type="button" className="w-full p-2 font-bold text-left text-gray-600 hover:bg-gray-100 active:bg-blue-100 active:border-l-4 active:border-l-blue-700 active:text-blue-700">
+                        <i className="mr-2 text-2xl bi bi-circle"></i>
                         Home
                     </button>
                 </li>
