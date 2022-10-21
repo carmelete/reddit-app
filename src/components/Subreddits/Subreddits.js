9a751df420a000f42162a6bcd4f9513d88b95347
Carmen Ortega Rico <carmenortegarico@gmail.com>
2022-10-21 13:10:55 +0200

    fix: center rows (#6)

--- src/components/Subreddits/Subreddits.js
+++ src/components/Subreddits/Subreddits.js
@@ -6,20 +6,8 @@ export function Subreddits() {
             <h2 className="mb-2 text-xl font-bold h-1/12">Subreddits</h2>
             <ul className="flex flex-col h-11/12">
                 <li>
-                    <button type="button" className="w-full p-2 font-bold text-left text-gray-600 hover:bg-gray-100 active:bg-blue-100 active:border-l-4 active:border-l-blue-700 active:text-blue-700">
-                        <i className="mr-2 text-2xl bi bi-circle"></i>
-                        Home
-                    </button>
-                </li>
-                <li>
-                    <button type="button" className="w-full p-2 font-bold text-left text-gray-600 hover:bg-gray-100 active:bg-blue-100 active:border-l-4 active:border-l-blue-700 active:text-blue-700">
-                        <i className="mr-2 text-2xl bi bi-circle"></i>
-                        Home
-                    </button>
-                </li>
-                <li>
-                    <button type="button" className="w-full p-2 font-bold text-left text-gray-600 hover:bg-gray-100 active:bg-blue-100 active:border-l-4 active:border-l-blue-700 active:text-blue-700">
-                        <i className="mr-2 text-2xl bi bi-circle"></i>
+                    <button type="button" className="flex items-center w-full p-2 font-bold text-left text-gray-600 border-l-4 border-transparent hover:bg-gray-100 active:bg-blue-100 active:border-l-4 active:border-l-blue-700 active:text-blue-700">
+                        <i className="flex items-center mr-2 text-2xl bi bi-circle"></i>
                         Home
                     </button>
                 </li>
