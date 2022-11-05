addbd68fdda3ac7c5b79eff62b4b7846a8f19d99
Carmen Ortega Rico <carmenortegarico@gmail.com>
2022-11-05 11:39:37 +0100

    feat: add search bar functionality (#12)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Card } from './components/Card/Card';
 import { Subreddits } from './components/Subreddits/Subreddits';
 import './App.css';
 import { useDispatch, useSelector } from "react-redux";
-import { fetchPosts } from './features/reddit/redditSlice';
+import { fetchPosts, selectFilteredPosts, setSearchTerm } from './features/reddit/redditSlice';
 import { fetchSubredditList, selectSubreddits } from './features/reddit/subRedditSlice';
 
 
@@ -14,24 +14,46 @@ function App() {
   const reddit = useSelector((state) => state.reddit);
   const { isLoading, error, searchTerm, selectedSubreddit } = reddit;
 
+  const posts = useSelector(selectFilteredPosts);
+
   useEffect(() => {
     dispatch(fetchPosts('pics'));
     dispatch(fetchSubredditList());
-  }, []);
+  }, [dispatch]);
+
+  const noPostMatching = () => {
+    return (
+      <div className="flex flex-col items-center mt-8">
+        <h2 className="mb-4 text-2xl font-bold text-center text-gray-500">
+          No posts matching "{searchTerm}"
+        </h2>
+        <button
+          type="button"
+          onClick={() => dispatch(setSearchTerm(''))}
+          className="w-32 h-12 text-white bg-orange-400 rounded-md"
+        >
+          Go home
+        </button>
+      </div>
+    );
+  }
+
 
   return (
     <div className="App">
       <Header />
       <div className="flex">
         <main className="w-9/12">
-          {
-            reddit.posts.map((post) =>
+          {posts.length > 0 &&
+            reddit.posts.map((post) => (
               <Card
                 key={post.id}
                 post={post}
-
               />
-            )
+            ))
+          }
+          {!posts.length &&
+            noPostMatching()
           }
         </main>
         <aside className="w-3/12">
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,56 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from '../../assets/images/reddit-logo.png';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSearchTerm } from '../../features/reddit/redditSlice';
 
 export function Header() {
+  const [searchTermlocal, setSearchTermLocal] = useState('');
+  const searchTerm = useSelector((state) => state.reddit.searchTerm);
+  const dispatch = useDispatch();
 
-    return (
-      <div className="flex h-16 shadow-lg">
-        <div className="flex items-center w-4/12 h-full" >
-          <img className="block w-auto h-10 ml-16 mr-2" src={logo} />
-          <p className="font-bold">
-            <span className="text-orange-400">Reddit</span>Minimal
-          </p>
-        </div>
-        <form className="flex items-center w-4/12" >
-            <input type="text" className="w-full px-4 py-1 mr-2 bg-gray-100 rounded-md" placeholder="Search"/>
-            <button type="submit" className="mb-1">
-                <i className="text-xl bi bi-search"></i>
-            </button>
-        </form>
-     </div>
-    );
-  }
+  const onSearchTermChange = (e) => {
+    setSearchTermLocal(e.target.value);
+  };
+
+  const onSearchTermSubmit = (e) => {
+    e.preventDefault();
+    dispatch(setSearchTerm(searchTermlocal));
+  };
+
+  useEffect(() => {
+    setSearchTermLocal(searchTerm);
+  }, [searchTerm]);
+
+  return (
+    <div className="flex h-16 shadow-lg">
+      <div className="flex items-center w-4/12 h-full" >
+        <img className="block w-auto h-10 ml-16 mr-2" src={logo} alt="logo"/>
+        <p className="font-bold">
+          <span className="text-orange-400">Reddit</span>Minimal
+        </p>
+      </div>
+      <form
+        className="flex items-center w-4/12"
+        onSubmit={onSearchTermSubmit}
+      >
+        <input
+          type="text"
+          className="w-full px-4 py-1 mr-2 bg-gray-100 rounded-md"
+          placeholder="Search"
+          onChange={onSearchTermChange}
+          value={searchTermlocal}
+        />
+        <button
+          type="submit"
+          className="mb-1"
+          onSubmit={onSearchTermSubmit}
+        >
+          <i className="text-xl bi bi-search"></i>
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Header;
